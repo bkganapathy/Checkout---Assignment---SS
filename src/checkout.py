e190e85e4c588fdb30d1f226df61371bc2a2d9c9
bkganapathy <2022mt93155@wilp.bits-pilani.ac.in>
2023-11-14 22:42:16 +0530

    Final code update to Checkout Microservice

--- src/checkout.py
+++ src/checkout.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from datetime import date
 import random
 import pymysql
+import requests
 
 app = Flask(__name__)
 
@@ -46,12 +47,12 @@ def insert_user():
 @app.route('/', methods=['POST','GET'])
 def process_order():
     print('/process_order:generating order_checkout.html')
-    data = request.args
-    user_name = data.get('user_name')
-    title = data.get('title')
-    author = data.get('author')
-    price = data.get('price')
-    #user_name = 'testing'    # Delete in final mode
+    data = request.args                 # Comment out this code when running in standalone mode
+    user_name = data.get('user_name')   # Comment out this code when running in standalone mode
+    title = data.get('title')           # Comment out this code when running in standalone mode
+    author = data.get('author')         # Comment out this code when running in standalone mode
+    price = data.get('price')           # Comment out this code when running in standalone mode
+    # user_name = 'testing'    # Delete in final mode
     c1 = user_name[:3]
     c2 = str(random.randint(1000, 9999))
     order_id = c1 + c2  # Generate a random order ID
@@ -59,9 +60,9 @@ def process_order():
     today = date.today()
     order_date = today.strftime("%d-%b-%Y")
 
-    #title = 'Whats a Good Name?'        # Delete in final mode
-    #author = 'John Roger'              # Delete in final mode
-    #price = 100.57                      # Delete in final mode
+    # title = 'Whats a Good Name?'        # Delete in final mode
+    # author = 'John Roger'              # Delete in final mode
+    # price = 100.57                      # Delete in final mode
 
     order_item = title + ' by ' + author
     order_amt = price
@@ -77,12 +78,27 @@ def process_order():
         y=x
         user_mobile = x[1]
         user_address = x[2]
-    print(x)
+    
     print(order_id, order_date, order_item, order_amt, user_name, f'Mobile No.{user_mobile}  Address : {user_address}')
+    
+    # Add order data into the order_data table 
+    insert_query = "INSERT INTO order_data (order_id, order_date, order_item, order_author, order_cost, order_c_name, order_address, order_mobile) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(insert_query, (order_id,today,title,author,price,user_name,user_address,user_mobile))
+    conn.commit()
+    print('Order details loaded successfully to order_data table.')
+    
     cursor.close()
     conn.close()
     
     return render_template('order_checkout.html', order_id=order_id, order_date=order_date, order_item=order_item, order_amt=order_amt, user_name=user_name, user_address=user_address, user_mobile=user_mobile)
+
+@app.route('/ret_prod/<user_name>')
+def ret_prod(user_name):
+    data1 = {'c_name': user_name}
+    # Send an HTTP GET request to the Product microservice Customer interface
+    response = requests.get('http://localhost:5001/products', params=data1)
+    return response.txt
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5002)
